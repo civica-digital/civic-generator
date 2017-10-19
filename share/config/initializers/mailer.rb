9c6eb86caa12389bd01fa6bcbccb03fdc45e75ef
Mr. Outis <mroutis@protonmail.com>
2017-10-19 13:36:50 -0500

    Add server region as an env var for AWS SES

--- share/config/initializers/mailer.rb
+++ share/config/initializers/mailer.rb
@@ -1,6 +1,7 @@
 if Rails.env.production?
   ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   access_key_id: ENV['AWS_ACCESS_KEY'],
-  secret_access_key: ENV['AWS_SECRET_KEY']
+  secret_access_key: ENV['AWS_SECRET_KEY'],
+  server: "email.#{ENV['AWS_REGION']}.amazonaws.com"
 end
 
