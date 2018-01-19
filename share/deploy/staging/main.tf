176902a2727721035da20049d3334f678e06035c
Mr. Outis <mroutis@protonmail.com>
2018-01-19 11:57:57 -0600

    Change the size to 1 virtual CPU and 1G RAM

--- share/deploy/staging/main.tf
+++ share/deploy/staging/main.tf
@@ -83,7 +83,7 @@ resource "digitalocean_droplet" "web" {
   image     = "ubuntu-16-04-x64"
   name      = "${var.project_name}"
   region    = "nyc3"
-  size      = "512mb"
+  size      = "s-1vcpu-1gb"
   ssh_keys  = ["${var.jenkins_ssh_fingerprint}"]
   user_data = "${data.template_file.setup_server.rendered}"
 }
